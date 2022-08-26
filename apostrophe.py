9f361b2ac92da5bd316902b39b1ed4d4e785861d
tran6ix <jvt2h@mtmail.mtsu.edu>
2022-08-25 21:53:28 -0500

    Learned f string, new line and tab formatting, and stripping white space, also why to use double apostrophe

---
+++ apostrophe.py
@@ -0,0 +1,5 @@
+from email import message_from_binary_file
+
+
+message = "Jordan's macbook"
+print(message)
--- full_name.py
+++ full_name.py
@@ -4,10 +4,12 @@ first_name = "jordan"
 last_name = "tran"
 full_name = f"{first_name} {last_name}"
 print(full_name)
+
 #by using f immediately before open quote, Python will replave each variable with its value within the string displayed
 #these are called f strings, the f is for format because Python formats the string by replacing the name of any vairable in braces with its value
 
 #Example 1
+
 first_name = "jordan"
 last_name = "tran"
 full_name = f"{first_name} {last_name}"
@@ -25,3 +27,38 @@ title = "Sorcer's Stone"
 author = "J.K Rowling"
 message = f"{title} by {author} is a great book!"
 print(message)
+#Whitespaces to strings with Tabs or new lines
+
+#Tabs
+print("\tPython")
+
+#NewLines
+print("Languages:\nPython\nC#\nJavaScript")
+
+print("\n")
+
+#Tabs and New Lines
+print("Languages:\n\tPython\n\tC#\n\tJavascript")
+
+#Stripping whitespace using a function
+#right strip
+language = 'python '
+print("Jordan likes",language,"very much")
+
+language = 'python '
+language =language.rstrip()
+print("Jordan likes",language,"very much")
+
+#left strip
+language = ' python'
+print("Jordan likes",language,"very much")
+
+language = ' python'
+language = language.lstrip()
+print("Jordan likes",language,"very much")
+
+#strip both sides
+language = ' python ' 
+language = language.strip()
+print("Jordan likes",language, "very much")
+
