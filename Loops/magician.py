b11916fd9afb8f154ee3deac2cc2e3ef0a8eab44
tran6ix <jvt2h@mtmail.mtsu.edu>
2022-08-29 22:52:03 -0500

    beginning of loops

---
+++ Loops/magician.py
@@ -0,0 +1,7 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+print('\n')
+tricks = ["hocus pocus", 'abracadabra', 'wingardium leviosa']
+for trick in tricks :
+    print(trick)
