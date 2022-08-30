52c1b4cc522b167576bcc65a02325229222f0eda
tran6ix <jvt2h@mtmail.mtsu.edu>
2022-08-29 22:38:01 -0500

    List

---
+++ Lists/bicycles.py
@@ -0,0 +1,15 @@
+bicycles = ['trek', 'mongoose', 'nike', 'adidas', 'redline']
+print(bicycles)
+
+bicycles = ['trek', 'mongoose', 'nike', 'adidas', 'redline']
+print(bicycles[0])
+
+bicycles = ['trek', 'mongoose', 'nike', 'adidas', 'redline']
+print(bicycles[0].title())
+print(bicycles[2])
+print(bicycles[4])
+#returns last item in the list 
+print(bicycles[-1])
+
+message = "My favorite bike is a"
+print(f"{message} {bicycles[2].title()} bike.")
---
+++ Lists/cars.py
@@ -0,0 +1,5 @@
+cars = ['bmw', 'audi', 'lexus', 'honda']
+print(sorted(cars))
+cars.reverse()
+print(cars)
+print(len(cars))
