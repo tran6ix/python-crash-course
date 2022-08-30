00589b9e0c7ea63bb65f1e827b1c16151205b1fa
tran6ix <jvt2h@mtmail.mtsu.edu>
2022-08-29 22:42:23 -0500

    Strings

---
+++ String Functions/name.py
@@ -0,0 +1,9 @@
+#Changing case in string with methods
+name = "jordan tran"
+print(name.title())
+#title function changes each word to upper-case
+name = "Jordan Tran"
+print(name.upper())
+print(name.lower())
+#upper does all leters in uppercase and vice versa
+#lower method is usually used to store data when you don't trust users with capitalization
---
+++ String Functions/name_cases.py
@@ -0,0 +1,29 @@
+#2-3 Personal Message
+
+
+name = "Jordan Tran"
+message = f"Hello {name}, would you like to learn Python?"
+print(message)
+
+print("\n")
+
+#2-4 Names Cases
+name = "jordan tran"
+print(name.lower())
+name = "ryan tran"
+print(name.upper())
+name = "andy tran"
+print(name.title())
+
+print("/n")
+
+#2-5 Quote
+#use \ to escape ' when using it as a string
+print("Zac Greer once said, \"If you can\'t face fear do it scared.\"")
+
+#2-6 Quote PT 2
+name = "Zac Greer"
+message = "once said, \"If you can\'t face fear do it scared.\""
+print(f"{name} {message}")
+
+
